fac2e8be10695b0b874a1310393d7b5b555510ab
Yuki992411 <k.yuki.9924.11@gmail.com>
2021-09-05 16:01:57 +0900

    APIを取得するコンポーネントを追加

---
+++ src/ApiHandler.js
@@ -0,0 +1,23 @@
+import Resas from "./.api_key.json";
+import { useState, useEffect } from "react";
+
+function ApiHandler(url = "", method = "GET") {
+  const [data, setdata] = useState([]);
+  useEffect(() => {
+    fetch(url, {
+      method: method,
+      headers: {
+        "Content-Type": "application/json;charset=UTF-8",
+        "X-API-KEY": Resas.apikey,
+      },
+    })
+      .then((res) => res.json())
+      .then((res_json) => {
+        setdata(res_json);
+      });
+  }, []);
+
+  return data;
+}
+
+export default ApiHandler;
