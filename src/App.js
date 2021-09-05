0965f70e26a583fdd5fbb417d4c99c8b998afe0c
Yuki992411 <k.yuki.9924.11@gmail.com>
2021-09-06 00:13:26 +0900

    チェックを入れた都道府県を抽出する処理を追加

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import GetPrefecture from "./GetPrefecture";
 
 function App() {
+  const [check, setcheck] = useState([]);
+
   const pref_names = GetPrefecture().map((v) => {
     return (
       <p key={`pref_${v.prefCode}`}>
         <label htmlFor={v.prefCode}>
-          <input type="checkbox" id={v.prefCode} />
+          <input
+            type="checkbox"
+            id={v.prefCode}
+            onClick={() => {
+              let update_check = { ...check };
+              update_check[v.prefCode] = !update_check[v.prefCode];
+              setcheck(update_check);
+            }}
+          />
           {v.prefName}
         </label>
       </p>
