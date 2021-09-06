a3a7017788e45a8b3ebd7ed064a347c9d0b6bb15
Yuki992411 <k.yuki.9924.11@gmail.com>
2021-09-06 18:29:23 +0900

    1回のリクエストにとどめるように修正， typeofによる判定に変更
    この修正により，urlの値が変わるときのみ，リクエストを発行する

--- src/GetPopulation.js
+++ src/GetPopulation.js
@@ -4,15 +4,20 @@ import { useState, useEffect } from "react";
 function GetPopulation(prefcode) {
   const [popudata, setpopudata] = useState({});
 
-  const url = `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=${prefcode}`;
+  const url_prefix =
+    "https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=";
+  let url;
+
+  // 2回以上のリクエスト発行を防いでいる
+  if (prefcode === 0 || typeof popudata[prefcode] !== "undefined") {
+    url = url_prefix + 0;
+  } else {
+    url = url_prefix + prefcode;
+  }
   const res_data = ApiHandler(url);
 
   useEffect(() => {
-    if (
-      res_data.result !== undefined &&
-      res_data.result !== null &&
-      prefcode > 0
-    ) {
+    if (typeof res_data.result !== "undefined" && res_data.result !== null) {
       let update_population_data = { ...popudata };
       update_population_data[prefcode] = res_data.result.data[0].data;
       setpopudata(update_population_data);
