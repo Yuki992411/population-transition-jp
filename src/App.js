e3a35dfe8d91ee663b53ed71a883a8fc4ec1774c
Yuki992411 <k.yuki.9924.11@gmail.com>
2021-09-06 15:44:18 +0900

    グラフ生成処理を追加

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 import "./App.css";
 import GetPrefecture from "./GetPrefecture";
 import GetPopulation from "./GetPopulation";
+import Graph from "./Graph";
 
 function App() {
   const [checked_num, setchecked_num] = useState(0);
   const [check, setcheck] = useState([]);
 
-  const pref_names = GetPrefecture().map((v) => {
+  const pref_data = GetPrefecture();
+  const pref_names = pref_data.map((v) => {
     return (
       <p key={`pref_${v.prefCode}`}>
         <label htmlFor={v.prefCode}>
@@ -28,13 +30,18 @@ function App() {
   });
 
   const population_data = GetPopulation(check[checked_num] ? checked_num : 0);
-  console.log(population_data);
-  console.log(check);
+  const plot_data = Object.keys(population_data)
+    .filter((keys) => check[keys] == true)
+    .map((keys) => ({
+      prefName: pref_data[keys - 1].prefName,
+      population: population_data[keys],
+    }));
 
   return (
     <div className="App">
       <h1>総人口推移グラフ</h1>
       <div>{pref_names}</div>
+      <Graph props={plot_data} />
     </div>
   );
 }
---
+++ src/Graph.js
@@ -0,0 +1,59 @@
+import React from "react";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+
+function Graph(props) {
+  const plot_data = props[Object.keys(props)].map((value) => {
+    const popu_data = value.population.map((sub_v) => sub_v.value);
+    return {
+      name: value.prefName,
+      data: popu_data,
+      pointStart: value.population[0].year,
+      pointInterval: value.population[1].year - value.population[0].year,
+    };
+  });
+  // console.log(plot_data);
+
+  const options = {
+    title: {
+      text: "",
+    },
+    chart: {
+      backgroundColor: {
+        linearGradient: [0, 0, 1, 1],
+        stops: [
+          [0, "rgb(240, 240, 255)"],
+          [1, "rgb(240, 240, 255)"],
+        ],
+      },
+      borderWidth: 1,
+      plotBackgroundColor: "rgba(255, 255, 255, .9)",
+      plotShadow: true,
+      plotBorderWidth: 1,
+    },
+    yAxis: {
+      title: {
+        text: "人口数（人）",
+      },
+      labels: {
+        formatter: function () {
+          return this.value / 10000 + " 万";
+        },
+      },
+    },
+    xAxis: {
+      title: {
+        text: "年",
+      },
+    },
+    series: plot_data,
+  };
+
+  return (
+    <>
+      <HighchartsReact highcharts={Highcharts} options={options} />
+    </>
+  );
+}
+
+export default Graph;
